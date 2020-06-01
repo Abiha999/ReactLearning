563bde0019a44e0e48b112f1e629a7aa29d7b923
abdoul gbadamassi <gbadhanane@gmail.com>
2020-06-01 19:57:26 +0000

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+
+
+
+function App()
+{
+  return(
+     <div>
+      <h1> My list </h1><br/>
+       <ul>
+         <li> pray</li>
+         <li>breakfast</li>
+         <li>shower</li>
+         <li>sleep</li>
+       </ul>
+       </div>
+       )
+}
+
+
+export default App;
